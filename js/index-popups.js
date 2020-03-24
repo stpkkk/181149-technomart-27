2c64447205dffb29164e0db037c128c9564eecbe
Stepanov Igor <stipyk1309@gmail.com>
2020-03-25 01:02:26 +0300

    Попытка 2

--- js/index-popups.js
+++ js/index-popups.js
@@ -12,6 +12,24 @@
   var mapclose = document.querySelector(".map-modal-close");
   var mappopup = document.querySelector(".modal-map");
   
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+    if (mappopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mappopup.classList.remove("modal-show");
+    }
+    }
+    });
+  
+  mapclose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mappopup.classList.remove("modal-show");
+  });
+  
+  maplink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mappopup.classList.add("modal-show");
+  });
   
   
   
@@ -112,27 +130,13 @@ cartlinks[8].addEventListener("click", function (evt) {
   cartpopup.classList.add("modal-show");
 });
 
-maplink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mappopup.classList.add("modal-show");
-});
 
 
 
 
-window.addEventListener("keydown", function(evt) {
-if (evt.keyCode === 27) {
-if (mappopup.classList.contains("modal-show")) {
-  evt.preventDefault();
-  mappopup.classList.remove("modal-show");
-}
-}
-});
 
-mapclose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  mappopup.classList.remove("modal-show");
-});
+
+
 
 
   
